5622370ed12bbd47c93382720d22589ec61c544e
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-08-30 19:43:25 +0530

    Update and rename adv_stars_class_app.py to home.py

---
+++ home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(page_title="Star Classification App", page_icon="🌟🔭", layout="wide")
+
+st.title("Welcome to the Star Classification App")
+st.markdown("""
+    This app allows you to classify celestial objects into three categories:
+    - **Galaxy**
+    - **QSO**
+    - **Star**
+
+    Use the navigation bar to explore different pages:
+    - **Home**: Introduction to the app.
+    - **Star Classification**: Input features to classify celestial objects.
+    - **About**: Learn more about the app and its development.
+    - **Contact**: Get in touch with us.
+""")
